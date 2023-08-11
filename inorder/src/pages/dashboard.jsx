079f872e7d73dea8a1ad3c79cdd912a0b5279056
DamiAdeeso <damilolaadeeso@gmail.com>
2023-08-11 22:16:04 +0100

    started dashboard

--- inorder/src/pages/dashboard.jsx
+++ inorder/src/pages/dashboard.jsx
@@ -8,16 +8,19 @@ function Dashboard() {
         <div id ="side-bar" ></div>
         <Container fluid>
             <Row>
-                <Col lg={3} style={{backgroundColor:"black", height:"100%"}} className='p-0 m-0'> 
-                <h3 className='mt-2 text-left'> Todo List</h3>
-                {
-                    {
-                        // const show ="true"
-                    }
-                }
+                <Col lg={3} id ="to-do" className='p-3 mr-4'> 
+                <h4 className='mt-1 text-left'> Todo List</h4>
+               <div className='mt-2'>
+                <div className='d-flex'>
+                <h5 className = "mr-5">Today</h5>
+                <p className='plus-minus' >+</p>
+                </div>
+                   
+                </div>     
+               
                 <div></div>
                 </Col>
-                <Col lg={9}>ghvjjkl</Col>
+                <Col lg={9}></Col>
             </Row>
         </Container>
       
